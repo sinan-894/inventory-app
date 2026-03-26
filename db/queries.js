7d3cadfee297ccd02b91b52ad08ea491ef455335
sinan-894 <mohammedsinan1011@gmail.com>
2026-03-26 12:33:45 +0400

    Add queries

--- db/queries.js
+++ db/queries.js
@@ -2,5 +2,14 @@ const pool = require('./pool')
 const {ITEM_SCHEMA} = require('../dataConfig')
 
 async function insertItems(items){
-    await pool.query('')    
+    const sql = Object.keys(ITEM_SCHEMA).join(',')
+    const values = items.join(',')
+    await pool.query('insert into items ($1) values ($2);',[sql,values])
+
 }
+
+async function getAllItems() {
+    const {rows} = await pool.query('select * from items;')
+    return rows
+}
+
