140900248a44f80260b560efe7fac5f714c14e9d
sinan-894 <mohammedsinan1011@gmail.com>
2026-03-29 03:11:41 +0400

    handle post request for items

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,21 +1,38 @@
 
 const {body,validationResult,matchedData}  = require('express-validator')
-const {ITEM_SCHEMA,getFormInputTag} = require('../dataConfig')
+const {ITEM_SCHEMA,VALIDATIONS,getFormInputTag} = require('../dataConfig')
 
 const items = []
 
+const itemsRows =  Object.keys(ITEM_SCHEMA)
 function getAllItems(req,res){
 
     res.render('items',{items:items})
 }
 
 function getItemCreateForm(req,res){
-    const rows =  Object.keys(ITEM_SCHEMA)
-    res.render('newItem',{items:rows,getTag:getFormInputTag})
+    res.render('newItem',{items:itemsRows,getTag:getFormInputTag})
 }
+
+
+const itemsValidations = itemsRows.map(rows=>{
+    return VALIDATIONS[rows]
+}) 
+function handleitemsCreatePost(req,res){
+    const error = validationResult(req)
+    if(!error.isEmpty()){
+        console.log('error')
+        console.log(error.array())
+    }
+    const data = matchedData(req)
+    console.log(data)
+}
+
+const itemsCreatePost  = [itemsValidations,handleitemsCreatePost]
 
 
 module.exports = {
     getAllItems,
     getItemCreateForm,
+    itemsCreatePost
 }
--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -1,10 +1,11 @@
 const {Router} = require('express')
 const itemsRouter = Router()
-const {getAllItems,getItemCreateForm} = require('../controllers/itemsController')
+const {getAllItems,getItemCreateForm,itemsCreatePost} = require('../controllers/itemsController')
 
 
 
 itemsRouter.get('/',getAllItems)
+itemsRouter.post('/',itemsCreatePost)
 itemsRouter.get('/new',getItemCreateForm)
 
 module.exports = {
