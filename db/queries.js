6f0fe578b3b2639c746eccb9cb64737ca7d0c377
sinan-894 <mohammedsinan1011@gmail.com>
2026-03-29 18:10:10 +0400

    Write queries

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,5 @@
 const pool = require('./pool')
-const {ITEM_SCHEMA} = require('../dataConfig')
+const {ITEM_SCHEMA,CATOGORY_SCHEMA} = require('../dataConfig')
 
 async function insertItems(items){
     const sql = Object.keys(ITEM_SCHEMA).join(',')
@@ -8,8 +8,44 @@ async function insertItems(items){
 
 }
 
+async function insertCategory(items) {
+    const sql = Object.keys(CATOGORY_SCHEMA).join(',')
+    const values = items.join(',') 
+    await pool.query('insert into items ($1) values ($2);',[sql,values])
+}
+
 async function getAllItems() {
     const {rows} = await pool.query('select * from items;')
     return rows
 }
 
+async function getAllCatogory() {
+    const { rows } = await pool.query('select * from catogory;')
+    return rows
+}
+
+
+async function getItemsOf(catogory) {
+    const {rows} = await pool.query('select * from items left join catogory on items.catogoryid=catogory.id where catogory.name=$1;',[catogory]) 
+    return rows
+    
+}
+
+async function getItem(itemName) {
+        
+    const {rows} = await pool.query(`
+            SELECT * from items
+            WHERE name=$1
+        `,[itemName])
+}
+
+
+module.exports = {
+    getAllCatogory,
+    getAllItems,
+    getItem,
+    getItemsOf,
+    insertItems,
+    insertCategory,
+    
+}
